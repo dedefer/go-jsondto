0bd4da0b7e47e4448878e692e1dfbc38b7176ad5
dedefer <ddf1998@gmail.com>
2022-06-08 01:32:30 +0300

    empty string for []byte(nil)

--- slice_map_empty.go
+++ slice_map_empty.go
@@ -1,6 +1,7 @@
 package jsondto
 
 import (
+	"reflect"
 	"unsafe"
 
 	jsoniter "github.com/json-iterator/go"
@@ -13,7 +14,10 @@ type nillableTypesEmptyOnNilExt struct {
 
 func (ext nillableTypesEmptyOnNilExt) DecorateEncoder(t reflect2.Type, e jsoniter.ValEncoder) jsoniter.ValEncoder {
 	if t, ok := t.(*reflect2.UnsafeSliceType); ok {
-		return sliceEmptyOnNilEncoder{ValEncoder: e, type_: t}
+		return sliceEmptyOnNilEncoder{
+			ValEncoder: e, type_: t,
+			bytes: t.Elem().Kind() == reflect.Uint8,
+		}
 	}
 
 	if t, ok := t.(*reflect2.UnsafeMapType); ok {
@@ -26,10 +30,16 @@ func (ext nillableTypesEmptyOnNilExt) DecorateEncoder(t reflect2.Type, e jsonite
 type sliceEmptyOnNilEncoder struct {
 	jsoniter.ValEncoder
 	type_ *reflect2.UnsafeSliceType
+	bytes bool
 }
 
 func (e sliceEmptyOnNilEncoder) Encode(ptr unsafe.Pointer, stream *jsoniter.Stream) {
 	if e.type_.UnsafeIsNil(ptr) {
+		if e.bytes {
+			stream.WriteString("")
+			return
+		}
+
 		stream.WriteEmptyArray()
 		return
 	}
